b85c29227b711ad1504e39b6c4267d6fc853ec55
‘Shelby <sls1881@gmail.com>
2021-04-15 11:11:38 -0700

    it works

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
-  github_username TEXT NOT NULL PRIMARY KEY
+  github_username TEXT NOT NULL PRIMARY KEY,
   github_photo_url TEXT NOT NULL
 );
 

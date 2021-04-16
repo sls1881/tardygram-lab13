e8e7f826ca39be694fddbe6e77a90ca07a757479
Eion Nelson <ecnelson1@live.com>
2021-04-15 17:54:58 -0700

    added verify and ensureAuth, added posts table, Post model, and posts  controller

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,17 @@
  
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users (
   github_username TEXT NOT NULL PRIMARY KEY,
   github_photo_url TEXT NOT NULL
 );
+CREATE TABLE posts (
+  id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  github_username TEXT NOT NULL,
+  post_photo TEXT NOT NULL,
+  post_caption TEXT NOT NULL,
+  post_tags TEXT NOT NULL
+);
+
 

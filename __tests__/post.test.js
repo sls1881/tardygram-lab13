656d86c7a0f3458eb9c43db3406773cd3a0ab6f4
Eion Nelson <ecnelson1@live.com>
2021-04-19 11:21:48 -0700

    added post tests for agg routes

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const User = require('../lib/models/User');
 const Post = require('../lib/models/Post');
+const Comment = require('../lib/models/Comment')
 
 jest.mock('../lib/middleware/ensureAuth.js', () => (req, res, next) => {
   req.user = {
@@ -19,21 +20,30 @@ describe('tardygram routes', () => {
     return setup(pool);
   });
 
+  let user;
+ let comment;
   let post;
   beforeEach(async () => {
+user = await User.insert({
+    username: 'test_user',
+    photoUrl: 'http://image.com/image.png'
+})
+
     post = await Post.insert({
       photo: 'http://pic.com/pic.png',
       caption: 'My cool pic',
       tags: 'fun',
+      username: user.username
     });
-  });
+
+    comment = await Comment.insert({
+      post:post.id,
+      commentBy: user.username,
+      comment:'Looks great!',
+    });
+  })
 
   it('Creates a post from test-user', async () => {
-    await User.insert({
-      username: 'test_user',
-      photoUrl: 'http://pic.com/pic.png',
-    });
-
     return request(app)
       .post('/api/v1/post')
       .send(post)
@@ -56,7 +66,18 @@ describe('tardygram routes', () => {
   it('Gets a post by ID', async () => {
     const res = await request(app).get(`/api/v1/post/${post.id}`);
 
-    expect(res.body).toEqual(post);
+    expect(res.body).toEqual({ 
+      id: '1',
+      photo: 'http://pic.com/pic.png',
+      caption: 'My cool pic',
+      tags: 'fun',
+      comments: [{
+        id: 1,
+        comment: 'Looks great!',
+        commentBy: 'test_user',
+        post: 1
+      }]});
+
   });
 
   it('Updates a post by ID', async () => {
@@ -86,3 +107,4 @@ describe('tardygram routes', () => {
     });
   });
 });
+
